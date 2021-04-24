19d76997ad407316c48fd3651235a75a66093bf7
Savannah Fortson <67602838+SavannahF@users.noreply.github.com>
2021-04-24 12:04:09 -0400

    class instructions test.js

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,22 @@
 // Manager.test.js
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// In addition to `Employee`'s properties and methods, `Manager` will also have the following:
+
+// * `officeNumber`
+
+// * `getRole()`&mdash;overridden to return `'Manager'`
