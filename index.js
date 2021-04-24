abb1a7f9da46ceed8611dd54f057e06755e98e07
Savannah Fortson <67602838+SavannahF@users.noreply.github.com>
2021-04-24 18:06:09 -0400

    html for output

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // index.js
+// test run
+// let test = "testName";
+// console.log(test);
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+// Choice between this or fs route??
+const generatePage = require("./src/template-page");
+const writeFile = require("./src/generate-site");
+
+let manager = [];
+let engineer = [];
+let intern = [];
+let employeeArr = { manager, engineer, intern };
+
+// let team = [];
