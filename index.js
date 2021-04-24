4626f5d2b66c6a0a939bb630e0ccf669c97177d0
Savannah Fortson <67602838+SavannahF@users.noreply.github.com>
2021-04-24 18:24:06 -0400

    making app.js html output skeleton from mocked up team.html

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 const fs = require("fs");
 const inquirer = require("inquirer");
+// const path = require("path");
 
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
@@ -15,9 +16,128 @@ const Manager = require("./lib/Manager");
 const generatePage = require("./src/template-page");
 const writeFile = require("./src/generate-site");
 
+// // or path module??
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const render = require("./lib/htmlRenderer");
+
 let manager = [];
 let engineer = [];
 let intern = [];
 let employeeArr = { manager, engineer, intern };
+// let employees = [];
 
 // let team = [];
+
+// Still need to put input prompts in this space..
+
+// Creating HTML document ./dist/team.html
+function startHtml() {
+  const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Teamwork Makes the Dream Work ;)</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Members</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+  fs.writeFile("./dist/team.html", html, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log("start");
+}
+
+function addHtml(member) {
+  return new Promise(function (resolve, reject) {
+    const name = member.getName();
+    const role = member.getRole();
+    const id = member.getId();
+    const email = member.getEmail();
+    let data = "";
+    if (role === "Engineer") {
+      const gitHub = member.getGithub();
+      data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}"
+                >${email}</a
+              >
+            </li>
+                <li class="list-group-item">GitHub: ${gitHub}<a
+                href="https://github.com/${gitHub}"
+                target="_blank"
+                rel="noopener noreferrer"
+                >${gitHub}</a
+              >
+            </li>
+            </ul>
+            </div>
+        </div>`;
+    } else if (role === "Intern") {
+      const school = member.getSchool();
+      data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}"
+                >${email}</a
+              >
+            </li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+    } else {
+      const officePhone = member.getOfficeNumber();
+      data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}"
+                >${email}</a
+              >
+            </li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`;
+    }
+    console.log("adding team member");
+    fs.appendFile("./dist/team.html", data, function (err) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve();
+    });
+  });
+}
+
+function finishHtml() {
+  const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+  fs.appendFile("./dist/team.html", html, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log("end");
+}
+
+initApp();
